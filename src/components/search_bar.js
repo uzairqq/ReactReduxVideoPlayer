437364191b3239116285cecbf6d5e7819d301f08
Uzair Iqbal <uzair.qq@outlook.com>
2018-11-20 02:44:53 +0500

    Handling User Event With Clean Code

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  onInputChange(event) {}
 
   render() {
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
 
