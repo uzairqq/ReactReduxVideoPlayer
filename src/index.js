1e8b3a6bf818d55bfb87a1d829c1c6f0562ba1c6
Uzair Iqbal <uzair.qq@outlook.com>
2019-05-25 01:54:41 +0500

    using customer props children

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetails from "./CommentDetails";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails
-        author="uzair"
-        timeAgo="Today at 5:00PM"
-        comment="No Need To it , just time waste"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetails
-        author="laraib"
-        timeAgo="Today at 4:45PM"
-        comment="Nice Blog Post"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetails
-        author="baba"
-        timeAgo="Yesterday at 2:00AM"
-        comment="I Love This Article"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetails
+          author="uzair"
+          timeAgo="Today at 5:00PM"
+          comment="No Need To it , just time waste"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="laraib"
+          timeAgo="Today at 4:45PM"
+          comment="Nice Blog Post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="baba"
+          timeAgo="Yesterday at 2:00AM"
+          comment="I Love This Article"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
