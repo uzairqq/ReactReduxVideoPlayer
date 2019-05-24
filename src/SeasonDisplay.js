16ad3ec917224ed7296f0b992bb1313fc7c4ff92
Uzair Iqbal <uzair.qq@outlook.com>
2019-05-25 03:21:39 +0500

    getting a user physical location

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return <div>hello there season display</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div />;
+  window.navigator.geolocation.getCurrentPosition(position =>
+    console.log(position, err => console.log(err))
+  );
+
+  return <div>hi there</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
