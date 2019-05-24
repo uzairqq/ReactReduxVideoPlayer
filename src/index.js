56d0a53f930c999e06192264fe03b6b521da690f
Uzair Iqbal <uzair.qq@outlook.com>
2019-05-25 00:09:37 +0500

    using faker fetched the image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a className="avatar" href="/">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
