5d4d37b141ea4eca0f410a2d200fb4f75d29df6c
Uzair Iqbal <uzair.qq@outlook.com>
2019-06-08 12:58:21 +0500

    converted main functional component into class based component and introduced state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(position =>
-    console.log(position, err => console.log(err))
-  );
-
-  return <div>hi there</div>;
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
