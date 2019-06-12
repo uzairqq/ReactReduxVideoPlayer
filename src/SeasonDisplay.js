f74d68d1a92f3d938284e7afe04bee85f7a37fb8
Uzair Iqbal <uzair.qq@outlook.com>
2019-06-12 16:08:48 +0500

    showing the icons with ternary expressions

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,7 +11,15 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === "winter" ? "Burr,it is chilly" : "Lets hit the beach";
-  return <div>{text}</div>;
+  const icon = text === "winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
