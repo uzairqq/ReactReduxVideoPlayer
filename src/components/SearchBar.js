b709338dddad5354c5d348a0de670f68c9ef529a
Uzair Iqbal <uzair.qq@outlook.com>
2019-06-14 16:55:30 +0500

    alternative of event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  handleChange(event) {
-    console.log(event.target.value);
-  }
-
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="feild">
             <label>Image Search</label>
-            <input type="text" onChange={this.handleChange} />
+            <input
+              type="text"
+              onChange={event => console.log(event.target.value)}
+            />
           </div>
         </form>
       </div>
