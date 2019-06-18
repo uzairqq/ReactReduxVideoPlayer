6dc18622932eef7c7a09117bbb7b862ffdb9b366
Uzair Iqbal <uzair.qq@outlook.com>
2019-06-19 00:50:21 +0500

    installed axios and used axios for fetching data based on search

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID d1524e66db2b5fae613b185ee021f7b6d752eecbad36a4a6e8711ec88a8662fd"
+      }
+    });
   }
 
   render() {
