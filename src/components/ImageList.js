c0dd790e9c719caa880d3d67f1b77668b7f04493
Uzair Iqbal <uzair.qq@outlook.com>
2019-06-19 03:09:16 +0500

    rendering list of images/components using .map

---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+
+const ImageList = props => {
+  const images = props.images.map(image => {
+    return <img src={image.urls.regular} />;
+  });
+
+  return <div>{images}</div>;
+};
+
+export default ImageList;
